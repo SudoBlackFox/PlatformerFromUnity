b6cff7a7c3926159925c67a230d248e046a22acf
SudoBlackFox <37751619+SudoBlackFox@users.noreply.github.com>
2020-01-24 16:57:35 +0300

    Add files via upload

---
+++ Assets/Dam_fire.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dam_fire : MonoBehaviour
+{
+    //private PlayerController pc;
+
+	void Start ()
+	{
+		StartCoroutine(Dest(3.5f));
+	}
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Game_Controller.damage();
+        StartCoroutine(Dest(3.5f));
+    }
+    private IEnumerator Dest(float value)
+	{
+		yield return new WaitForSeconds(value);
+		Destroy(gameObject);
+	}
+}
